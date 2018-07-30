b30176400f8394689a38ad8e01ee4d6187ed0198
Bilger Yahov <bayahov1@gmail.com>
2018-07-30 16:24:20 +0200

    Add some styling.

--- MyProject/app/home/home.component.ts
+++ MyProject/app/home/home.component.ts
@@ -3,7 +3,8 @@ import { Component } from "@angular/core";
 @Component({
     selector: "ns-home",
     moduleId: module.id,
-    templateUrl: "./home.component.html"
+    templateUrl: "./home.component.html",
+    styleUrls:["./home.component.css"]
 })
 
 export class HomeComponent { }
