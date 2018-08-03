745bcc2064f4e8d18d97e59f2bf8d7b465a99a1a
Bilger Yahov <bayahov1@gmail.com>
2018-08-03 16:17:58 +0300

    Add some info on an array and output that on the screen.

--- MyProject/app/components/home/home.component.ts
+++ MyProject/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
     selector: "ns-home",
@@ -7,4 +7,22 @@ import { Component } from "@angular/core";
     styleUrls:["./home.component.css"]
 })
 
-export class HomeComponent { }
+export class HomeComponent implements OnInit {
+
+    public people: Array<any>;
+
+    public constructor() {
+        this.people = [];
+    }
+
+    public ngOnInit(): void {
+        this.people.push({
+            "firstName": "Bilger",
+            "lastName": "Yahov"
+        });
+        this.people.push({
+            "firstName": "Ilker",
+            "lastName": "Yahov"
+        });
+    }
+ }
