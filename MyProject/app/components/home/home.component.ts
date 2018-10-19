b7d3b8200b58c404028ca39fa351d4df0dea856c
Bilger Yahov <bilger.yahov@progress.com>
2018-10-19 16:46:02 +0300

    Access native platform features to get the application version.

--- MyProject/app/components/home/home.component.ts
+++ MyProject/app/components/home/home.component.ts
@@ -2,7 +2,13 @@ import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { Couchbase } from "nativescript-couchbase";
 import { Location } from "@angular/common";
+import * as Platform from "platform";
+import * as Application from "application";
 const jsSHA = require("jssha") ;
+
+declare var android: any;
+declare var java: any;
+declare var NSBundle: any;
 
 @Component({
     selector: "ns-home",
@@ -43,6 +49,18 @@ export class HomeComponent implements OnInit {
 
     public navigateCreatePage() {
         this.router.navigate(["create"]);
+    }
+
+    public getApplicationVersion () : string {
+        if (Platform.isAndroid) { 
+            let PackageManager = android.content.pm.PackageManager;
+            let pkg = Application.android.context.getPackageManager()
+                .getPackageInfo(Application.android.context.getPackageName(), PackageManager.GET_META_DATA);
+            return java.lang.Integer.toString(pkg.versionCode);
+        } else {
+             let version = NSBundle.mainBundle.objectForInfoDictionaryKey("CFBundleShortVersionString");
+             return version;
+        }
     }
 
     private loadData () {
